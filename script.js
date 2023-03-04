977fa70126915c5182fe7bc80c3ba232b88af4b6
Rohan Patil <rohanpatil4002@gmail.com>
2023-03-05 04:13:54 +0530

    logorg

--- script.js
+++ script.js
@@ -159,3 +159,31 @@ document.querySelector('#allBtn').addEventListener('click', function() {
         element.style.display = 'grid';
     });
 })
+
+// Search
+// Get references to the search box and search results elements
+// const searchBox = document.getElementById('search-box');
+// const searchResults = document.getElementById('search-results');
+
+// // Add an event listener to the search box to handle search input
+// searchBox.addEventListener('input', () => {
+//   // Get the search query entered by the user
+//   const query = searchBox.value.toLowerCase();
+
+//   // Clear the search results container
+//   searchResults.innerHTML = '';
+
+//   // Loop through all the elements that need to be searched
+//   const elementsToSearch = document.querySelectorAll('p, h1, h2, h3');
+//   for (let i = 0; i < elementsToSearch.length; i++) {
+//     const element = elementsToSearch[i];
+
+//     // Check if the element contains the search query
+//     if (element.textContent.toLowerCase().includes(query)) {
+//       // If the element contains the search query, add it to the search results container
+//       const result = document.createElement('div');
+//       result.innerHTML = element.outerHTML;
+//       searchResults.appendChild(result);
+//     }
+//   }
+// });
